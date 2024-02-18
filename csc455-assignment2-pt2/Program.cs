dc13cb8d178851e43c9877ae160bbe3cb4066dda
hunterw455 <hunter.wilkinson@trojans.dsu.edu>
2024-02-17 23:26:46 -0600

    Add the display menu to the code, but I used functions instead.

--- csc455-assignment2-pt2/Program.cs
+++ csc455-assignment2-pt2/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//  Assignment 1 - Program performs 4 different features based on the user's input.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +8,33 @@ using System.Threading.Tasks;
 
 namespace csc455_assignment2_pt2
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  Display menu and get user input
+            displayMenu();
+            string userInput = Console.ReadLine();
+            
+            
+            //  Used so the cmd didn't exit quickly
             Console.ReadLine();
+        }
+
+        static void displayMenu()
+        {
+            Console.WriteLine("Choose a feature you'd like this program to run by entering a number between 1 and 4.");
+            Console.WriteLine("     1 - Random Positive Integer Between 1 And 10");
+            Console.WriteLine("     2 - Output Today's Date In Short Date String");
+            Console.WriteLine("     3 - Output A Random Dinosaur Name");
+            Console.WriteLine("     4 - Perform A Random Action On A String");
+        }
+
+        //  Displays a random integer based on the user's input
+        static void randomInt()
+        {
+            Random randomNum = new Random();
+            Console.WriteLine("Here is a random integer number between 1 and 10: {0}", randomNum.Next(1, 11));
         }
     }
 }
