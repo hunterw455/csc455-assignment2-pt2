003b3c238d2ab31563b8bba04872bdb0899c34e5
hunterw455 <hunter.wilkinson@trojans.dsu.edu>
2024-02-18 12:01:20 -0600

    Added the test for the 4th method the program will perform

--- csc455-assignment2-pt2.Tests/UnitTest1.cs
+++ csc455-assignment2-pt2.Tests/UnitTest1.cs
@@ -46,11 +46,57 @@ namespace csc455_assignment2_pt2.Tests
             Assert.IsTrue(str.Contains(expected));
         }
 
-        //  Tests if the user inputs an invalid input
+        //  Tests the fourth method the program will perform
+        [TestMethod]
+        public void testMethod4()
+        {
+            //  Arrange
+            string expected = "";
+            MenuOptions menuOptions = new MenuOptions();
+            //  Act
+            string str = menuOptions.processChoice("4");
+            //  Assert
+            if (str.Contains("Your string in reverse is"))
+            {
+                Assert.IsTrue(str.Contains("Your string in reverse is"));
+            } else if (str.Contains("The length of your string is"))
+            {
+                Assert.IsTrue(str.Contains("The length of your string is"));
+            } else if (str.Contains("Half of your string is"))
+            {
+                Assert.IsTrue(str.Contains("Half of your string is"));
+            } else if (str.Contains("Your string in all uppercase values is"))
+            {
+                Assert.IsTrue(str.Contains("Your string in all uppercase values is"));
+            } else if (str.Contains("If we replace any letter e with an _, your string will look like"))
+            {
+                Assert.IsTrue(str.Contains("If we replace any letter e with an _, your string will look like"));
+            } else if (str.Contains("The hash code of your string is"))
+            {
+                Assert.IsTrue(str.Contains("The hash code of your string is"));
+            } else if (str.Contains("Your string in all lowercase values is"))
+            {
+                Assert.IsTrue(str.Contains("Your string in all lowercase values is"));
+            } else if (str.Contains("Your two strings together are"))
+            {
+                Assert.IsTrue(str.Contains("Your two strings together are"));
+            } else if (str.Contains("word(s) in your string"))
+            {
+                Assert.IsTrue(str.Contains("word(s) in your string"));
+            } else if (str.Contains("spaces in your string"))
+            {
+                Assert.IsTrue(str.Contains("spaces in your string"));
+            }
+        }
+
+
+        //  Tests if the user inputs an invalid choice
         [DataTestMethod]
         [DataRow("")]
         [DataRow(" ")]
-        [DataRow(34)]
+        [DataRow("34")]
+        [DataRow("\t")]
+        [DataRow("-10000000000000000000000000000000000")]
         public void testMethodInvalidInputs(string input)
         {
             //  Arrange
@@ -61,18 +107,6 @@ namespace csc455_assignment2_pt2.Tests
             //  Assert
             Assert.IsTrue(str.Contains(expected));
         }
-        [TestMethod]
-        public void testMethodInvalidInput1()
-        {
-            //  Arrange
-            string expected = "Input a valid integer number between 1 and 4.";
-            MenuOptions menuOptions = new MenuOptions();
-            //  Act
-            string str = menuOptions.processChoice("");
-            //  Assert
-            Assert.IsTrue(str.Contains(expected));
-        }
-
     }
 }
 
