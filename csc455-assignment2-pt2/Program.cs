166cbd41f21e8284db24a9b1395ef581945ddc22
hunterw455 <hunter.wilkinson@trojans.dsu.edu>
2024-02-17 23:09:18 -0600

    Add project files.

---
+++ csc455-assignment2-pt2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csc455_assignment2_pt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
